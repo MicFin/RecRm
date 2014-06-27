978c258cede1bf3a821741f59bef3149899d6145
Michael Finneran <mrfinneran@gmail.com>
2014-06-27 19:32:25 -0400

    missed on usage of Attribute, found it
    quit
    \q

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,5 +3,5 @@ class Ingredient < ActiveRecord::Base
 	has_many :ingredients_recipes
 	has_many :recipes, through: :ingredients_recipes
 	has_and_belongs_to_many :recipe_steps
-  has_and_belongs_to_many :attributes
+  has_and_belongs_to_many :characteristics
 end

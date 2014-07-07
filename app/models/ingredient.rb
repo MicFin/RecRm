08c7b4b0d253cbf22aba996d7fe41ba0b23a4159
Michael Finneran <mrfinneran@gmail.com>
2014-07-07 01:32:32 -0400

    added polymorphic to models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -5,6 +5,7 @@ class Ingredient < ActiveRecord::Base
 	has_and_belongs_to_many :recipe_steps
 	has_and_belongs_to_many :allergens
 	has_and_belongs_to_many :characteristics
+  has_many :quality_reviews, as: :quality_reviewable 
   # method to check if an ingredient has had allergens added
   def need_allergens?
     # if an ingredient has allergens already

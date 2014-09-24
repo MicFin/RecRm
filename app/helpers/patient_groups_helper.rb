333902d2b18eccbf19245ca7857b31738fcfafee
Michael Finneran <mrfinneran@gmail.com>
2014-09-24 13:57:03 -0400

    reciep category view

--- app/helpers/patient_groups_helper.rb
+++ app/helpers/patient_groups_helper.rb
@@ -1,9 +1,9 @@
 module PatientGroupsHelper
 
   def get_patient_groups!
-    @diseases = PatientGroup.where(category: "disease", input_option: false)
-    @intolerances = PatientGroup.where(category: "intolerance", input_option: false)
-    @allergies = PatientGroup.where(category: "allergy", input_option: false)
+    @diseases = PatientGroup.where(category: "disease", input_option: nil)
+    @intolerances = PatientGroup.where(category: "intolerance", input_option: nil)
+    @allergies = PatientGroup.where(category: "allergy", input_option: nil)
   end
 
 end

1d6c5ccebaea0e40ff3083fe3875a27f2f1542e6
Michael Finneran <mrfinneran@gmail.com>
2014-09-24 12:07:41 -0400

    Adding validation to conflict form, in progress

--- app/controllers/quality_reviews_controller.rb
+++ app/controllers/quality_reviews_controller.rb
@@ -3,9 +3,11 @@ class QualityReviewsController < ApplicationController
   before_filter :set_quality_review, only: [:update, :edit]
 
   def new
-    @quality_review = QualityReview.new 
     @recipe_id = params[:recipe_id]
     @recipe = Recipe.find(@recipe_id)
+    @quality_review = QualityReview.new(dietitian_id: current_dietitian.id, quality_reviewable_id: @recipe.id, quality_reviewable_type: "Recipe", date_due_by: DateTime.now+14)
+    @quality_review.save 
+    ## create or use helpers for these
     @ingredients = @recipe.ingredients_recipes
     @steps = @recipe.recipe_steps
     @health_groups = @recipe.patient_groups
@@ -13,6 +15,36 @@ class QualityReviewsController < ApplicationController
     @marketing_items_by_group = @recipe.marketing_items_by_group
     @ingredients_tagged = @recipe.ingredients_tagged
     @ingredients_not_tagged = @recipe.ingredients_not_tagged
+    # create methods on recipe model and helpers to get these
+    # then put them in review_conflict_controller and have qulaity_review make ajax calls for forms when clicked
+    # for now this should do until further code can be written
+    @basic_info_review_conflicts = {
+      "name" =>
+        ReviewConflict.new(risk_level: 100, category: "basic_info", item: "name", review_stage: 100, first_reviewer_id: current_dietitian.id),
+      "cook_time" =>
+      ReviewConflict.new(risk_level: 100, category: "basic_info", item: "cook_time", review_stage: 100, first_reviewer_id: current_dietitian.id),
+      "prep_time" =>
+      ReviewConflict.new(risk_level: 100, category: "basic_info", item: "prep_time", review_stage: 100, first_reviewer_id: current_dietitian.id),
+      "serving_size" =>
+      ReviewConflict.new(risk_level: 100, category: "basic_info", item: "serving_size", review_stage: 100, first_reviewer_id: current_dietitian.id),
+      "difficulty" =>
+      ReviewConflict.new(risk_level: 100, category: "basic_info", item: "difficulty", review_stage: 100, first_reviewer_id: current_dietitian.id)
+    }
+    @ingredient_review_conflicts = {}
+    @recipe.ingredients_recipes.each do | ingredient |
+      @ingredient_review_conflicts["ingredient-#{ingredient.id}"] = ReviewConflict.new(risk_level: 100, category: "ingredient", item: "ingredient-#{ingredient.id}", review_stage: 100, first_reviewer_id: current_dietitian.id)
+    end
+    @step_review_conflicts = {}
+    @recipe.recipe_steps.each do | step |
+      @step_review_conflicts["step-#{step.id}"] = ReviewConflict.new(risk_level: 100, category: "step", item: "step-#{step.id}", review_stage: 100, first_reviewer_id: current_dietitian.id)
+    end
+    @allergen_review_conflicts = {}
+    @recipe.ingredients.each do | allergen |
+      @allergen_review_conflicts["allergen-#{allergen.id}"] = ReviewConflict.new(risk_level: 100, category: "allergen", item: "allergen-#{allergen.id}", review_stage: 100, first_reviewer_id: current_dietitian.id)
+    end
+    @recipe_tag_review_conflicts = {}
+    @recipe_tag_review_conflicts["health_group"] = ReviewConflict.new(risk_level: 1001, category: "health_group", item: "prep_time", review_stage: 100, first_reviewer_id: current_dietitian.id)
+    @recipe_tag_review_conflicts["recipe_categories"] = ReviewConflict.new(risk_level: 100, category: "recipe_categories", review_stage: 100, first_reviewer_id: current_dietitian.id)
   end
 
   def edit
--- app/controllers/review_conflicts_controller.rb
+++ app/controllers/review_conflicts_controller.rb
@@ -10,10 +10,22 @@ class ReviewConflictsController < ApplicationController
   end
 
   def create
-
+    binding.pry
+    @review_conflict = ReviewConflict.new(review_conflict_params)
+    @recipe = Recipe.find(params[:recipe_id])
+    respond_to do |format|
+      if @review_conflict.save
+        format.html { redirect_to new_review_conflict_path(review_conflict_id: @review_conflict.id), notice: 'review_conflict was successfully created.' }
+        format.json { render :show, status: :created, location: @review_conflict }
+        format.js
+      else
+        format.html { render :new }
+        format.json { render json: @review_conflict.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  # PATCH/PUT /articles/:id/review_conflict/:id
+  # PATCH/PUT /review_conflicts/:id/review_conflict/:id
   # PATCH/PUT /articles/:id/review_conflict/:id.json
   def update
 
@@ -27,10 +39,11 @@ class ReviewConflictsController < ApplicationController
   private
 
   def review_conflict_params
-    params.require(:review_conflict).permit(:passed, :dietitian_id, :recipe_id, :completed)
+    params.require(:review_conflict).permit(:first_reviewer_id, :second_reviewer_id, :third_reviewer_id, :risk_level, :category, :item, :issue, :resolved, :review_stage, :quality_review_id, :first_suggestion, :second_suggestion, :third_suggestion)
   end
 
   def set_review_conflict
+    binding.pry
     @review_conflict = ReviewConflict.find(params[:id])
   end
 

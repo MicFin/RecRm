e8b45b6cf4b12e2fca729f4e4e3c906bab8aa186
Michael Finneran <mrfinneran@gmail.com>
2014-09-30 16:05:28 -0400

    working on imssing csid and edited patient groups

--- app/helpers/patient_groups_helper.rb
+++ app/helpers/patient_groups_helper.rb
@@ -1,7 +1,7 @@
 module PatientGroupsHelper
 
   def get_patient_groups!
-    @diseases = PatientGroup.where(category: "disease", input_option: nil)
+    @diseases = PatientGroup.where(category: "disease")
     @intolerances = PatientGroup.where(category: "intolerance", input_option: nil)
     @allergies = PatientGroup.where(category: "allergy", input_option: nil)
     @diets = PatientGroup.where(category: "Diet").order(:order)

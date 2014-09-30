8744fb3e462579b34c8d09e97701b45af8c32e93
Michael Finneran <mrfinneran@gmail.com>
2014-09-30 16:36:17 -0400

    working on imssing csid and edited patient groups

--- app/helpers/characteristics_helper.rb
+++ app/helpers/characteristics_helper.rb
@@ -1,13 +1,13 @@
 module CharacteristicsHelper
 
     def get_recipe_characteristics!
-      @cook_times = Characteristic.where(category: "Cook Time")
+      @cook_times = Characteristic.where(category: "Cook Time").order(:order)
       @cook_count = @cook_times.count
-      @prep_times = Characteristic.where(category: "Prep Time")
+      @prep_times = Characteristic.where(category: "Prep Time").order(:order)
       @prep_count = @prep_times.count
-      @difficulties = Characteristic.where(category: "Difficulty")
+      @difficulties = Characteristic.where(category: "Difficulty").order(:order)
       @difficulty_count = @difficulties.count
-      @serving_sizes = Characteristic.where(category: "Serving Size")
+      @serving_sizes = Characteristic.where(category: "Serving Size").order(:order)
       
       @courses = Characteristic.where(category: "Course").order(:order)
       @meals = Characteristic.where(category: "Meal").order(:order)

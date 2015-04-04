188c4d7872a70b640f9aaffe56f0f59ca83dadad
Michael Finneran <mrfinneran@gmail.com>
2015-04-04 09:32:38 -0400

    temporary fix for manually adding a room to an appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -284,13 +284,33 @@ class AppointmentsController < ApplicationController
   # PATCH/PUT /appointments/1
   # PATCH/PUT /appointments/1.json
   def update
-    
     if @appointment.status == "Follow Up Unpaid" 
       clean_dates_for_database
     end
     # if update saves
-    if @appointment.update(appointment_params)
+
+
+    ##### added for manually assigning a room
+    if params[:new_room_needed] == "true"
+        
+        @new_session = @opentok.create_session 
+        @tok_token = @new_session.generate_token :session_id =>@new_session.session_id 
+        ## creating a new room each time, should either purge old rooms or assign rooms to dietitians 
+        @new_room = Room.new(dietitian_id:  @appointment.dietitian_id, public: true, sessionId: @new_session.session_id, name: "Early Access Session")
+        @new_room.save
+        dietitian = @appointment.dietitian
+        dietitian.add_role "Session Host", @new_room
+        user = @appointment.appointment_host
+        user.add_role "Session Guest", @new_room
+        user.save
+        # set appointment to room (1st and only for now)
+        if @appointment.room_id == nil
+          @appointment.room_id = @new_room.id
+        end
+    ##### should remove above
+    elsif @appointment.update(appointment_params)
       # if stripe card payment update incnluded in update then user is paying 
+
       if appointment_params[:stripe_card_token]
         # pay for appointment
         token = appointment_params[:stripe_card_token]
@@ -308,7 +328,7 @@ class AppointmentsController < ApplicationController
       elsif params[:appointment][:note]
 
       # or when admin dietitian assigns dietitian
-      elsif @appointment.dietitian_id != nil
+      elsif (@appointment.dietitian_id != nil)
         ## assumes that appointment has been paid for and assigned a dietitian by admin dietitian
         @new_session = @opentok.create_session 
         @tok_token = @new_session.generate_token :session_id =>@new_session.session_id 

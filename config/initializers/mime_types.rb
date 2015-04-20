10a96473253270acc981961d83a3230917bc2cbc
Michael Finneran <mrfinneran@gmail.com>
2015-04-20 10:29:40 -0400

    Added mime type for excel

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,7 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+
+# mime type for excel.  Use for exporting data
+# http://railscasts.com/episodes/362-exporting-csv-and-excel?view=asciicast
+Mime::Type.register "application/xls", :xls

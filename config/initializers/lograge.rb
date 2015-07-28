a91bd0214998bda47e24a2504c212cefcf56ac6e
Michael Finneran <mrfinneran@gmail.com>
2015-07-28 02:38:17 -0400

    customized lograge to show timestamp and params of request

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -3,6 +3,19 @@ Rails.application.configure do
 
   # add time to lograge
   config.lograge.custom_options = lambda do |event|
-    {:time => event.time}
+    # {:time => event.time}
+     params = event.payload[:params].reject do |k|
+      ['controller', 'action'].include? k
+    end
+
+    { "params" => params, :time => event.time }
   end
+
+  # config.lograge.custom_options = lambda do |event|
+  #   params = event.payload[:params].reject do |k|
+  #     ['controller', 'action'].include? k
+  #   end
+
+  #   { "params" => params }
+  # end
 end

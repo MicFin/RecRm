22477b6edc8b57ef4f9e9906cefb12cbdda2ecad
Michael Finneran <mrfinneran@gmail.com>
2015-11-15 21:00:33 -0500

    Create appointment.js

---
+++ app/assets/javascripts/appointments.js
@@ -0,0 +1,12 @@
+// Controller specific JS to be executed for appointments controller
+
+//= require appointments/appointment_prep
+
+// when page has loaded...
+$(document).ready(function() {
+
+	// JS for appointments#appointment_prep
+	Kindrdfood.appointments.appointmentPrep.init();
+});
+
+

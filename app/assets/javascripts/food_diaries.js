e28f079878217650ed0b5efca308469b6ce42099
Michael Finneran <mrfinneran@gmail.com>
2015-11-16 23:38:45 -0500

    Add food diary js

--- app/assets/javascripts/food_diaries.js
+++ app/assets/javascripts/food_diaries.js
@@ -3,12 +3,12 @@
 // Food Diaries Controller JS
 
 //= require food_diaries/show
-
+//= require food_diaries/new
 
 	
 $(document).ready(function() {
 
 	Kindrdfood.foodDiaries.show.init();
 
-
+	Kindrdfood.foodDiaries.new.init();
 });
---
+++ app/assets/javascripts/food_diaries/new.js
@@ -0,0 +1,13 @@
+
+Kindrdfood = Kindrdfood || {};
+
+
+Kindrdfood.foodDiaries = Kindrdfood.foodDiaries || {};
+
+Kindrdfood.foodDiaries.new = {
+  init: function(){
+
+  	Kindrdfood.ajaxSpecific.ajaxUpdateUrl.init();
+		Kindrdfood.formValidations.foodDiaryEntries.init();
+	}
+}

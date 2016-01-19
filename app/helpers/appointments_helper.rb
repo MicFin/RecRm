e6e398b41685ed959bd010f734d97b908aa0690b
Michael Finneran <mrfinneran@gmail.com>
2016-01-18 19:46:48 -0500

    cleaned up and commented helpers

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -1,15 +1,28 @@
 module AppointmentsHelper
 
+  # Get upcoming appointments for a dietitian or client, and next appointment for dietitian
   def get_upcoming_appointments!
+
+    # Create upcoming appointments array
     @upcoming_appointments = []
+
+    # Set user to current dietitian or current user
     user = current_dietitian || current_user
+
+    # If user is an Admin Dietitian then get all upcoming appointments
     if user.has_role? "Admin Dietitian"
       list_of_appointments = Appointment.where("start_time > ?", DateTime.now).order('start_time ASC, created_at ASC')
+
+    # If user is a client then get all of their upcoming appointments
     elsif user.is_a? User 
       list_of_appointments = user.appointment_hosts.where("start_time > ?", DateTime.now).order('start_time ASC, created_at ASC')
+
+    # If user is neither a client or Admin Dietitian then is a dietitian so get all of their upcoming appointments
     else
       list_of_appointments = user.appointments.where("start_time > ?", DateTime.now).order('start_time ASC, created_at ASC')
     end
+
+    # Build family and add to appointments
     list_of_appointments.map do |appointment| 
       family = appointment.appointment_host.head_of_families.last 
       family.health_groups_names = family.health_groups.map(&:name)
@@ -17,44 +30,37 @@ module AppointmentsHelper
       family.number_of_members = family.family_member_count
       family.family_names = family.all_first_names
       appointment.family_info = family
+
+      # if prep is complete then mark as prepped
       appointment.prepped = appointment.dietitian_prep_complete?
+
+      # add to upcoming appointments array
       @upcoming_appointments << appointment
     end
+
+    # Group upcoming appoinments by data and time
     @upcoming_appointments = @upcoming_appointments.group_by{|appointment|  [appointment.start_time.in_time_zone("Eastern Time (US & Canada)").strftime("%B %d, %Y"), appointment.start_time.in_time_zone("Eastern Time (US & Canada)").strftime("%I:%M%p")] }
 
+    # If user is a dietitian then set next appointment, add family and mark if prepped
     if user.is_a? Dietitian 
       @next_appointment = user.appointments.where(status: "Paid").where(start_time: 30.minutes.ago..5.hours.from_now).last
-      #@next_appointment = user.appointments.where(status: "Paid").last
-    end
-    # @next_appointment = user.appointments.where.not(status: "Follow Up Unpaid").last
-
-    if @next_appointment != nil  
-      family = @next_appointment.appointment_host.head_of_families.last 
-      family.health_groups_names = family.health_groups.map(&:name)
-      family.age_groups = family.ages
-      family.number_of_members = family.family_member_count
-      family.family_names = family.all_first_names
-      @next_appointment.family_info = family
-      @next_appointment.prepped = @next_appointment.dietitian_prep_complete?
-    end
-  end
-
-  def get_upcoming_appointment!
-      # client upcoming appointment or clirent unpaid
-
-      @upcoming_appointment = current_user.appointment_hosts.where("start_time > ?", DateTime.now).where(status: "Paid").last || current_user.appointment_hosts.where("start_time > ?", DateTime.now).where(status: "Follow Up Unpaid").last
-
-      # start date and time 
-      if @upcoming_appointment 
-        @upcoming_appointment.date = @upcoming_appointment.start_time.strftime("%A, %b %d") unless @upcoming_appointment.nil?
-        @upcoming_appointment.time = @upcoming_appointment.start_time.strftime("%I:%M%p") unless @upcoming_appointment.nil?
-
+      if @next_appointment != nil  
+        family = @next_appointment.appointment_host.head_of_families.last 
+        family.health_groups_names = family.health_groups.map(&:name)
+        family.age_groups = family.ages
+        family.number_of_members = family.family_member_count
+        family.family_names = family.all_first_names
+        @next_appointment.family_info = family
+        @next_appointment.prepped = @next_appointment.dietitian_prep_complete?
       end
+    end
   end
 
+  # Get previous appointments (marked as Complete) for a dietitian or client
   def get_previous_appointments!
     @previous_appointments = []
-    # if dietitian then get all previous appointments
+
+    # if dietitian then get all previous appointments and add family
     if current_dietitian
       current_dietitian.appointments.map do |appointment| 
         if appointment.status == "Complete"
@@ -68,7 +74,8 @@ module AppointmentsHelper
           @previous_appointments << appointment
         end
       end
-    # if not dietitian then user so get all previous appointment hosts
+
+    # if not dietitian then user so get all previous appointment hosts and add family
     else
       current_user.appointment_hosts.map do |appointment| 
         if appointment.status == "Complete"
@@ -84,15 +91,30 @@ module AppointmentsHelper
       end
     end
 
-    # group previous appointmetns by time
+    # group previous appointments by date and time
     @previous_appointments = @previous_appointments.group_by{|appointment|  [appointment.start_time.in_time_zone("Eastern Time (US & Canada)").strftime("%B %d, %Y"), appointment.start_time.in_time_zone("Eastern Time (US & Canada)").strftime("%I:%M%p")] }
   end
 
+  # Get upcoming appointment for client
+  def get_upcoming_appointment!
+
+    # Get last paid appointment or last follow up unpaid appointment
+      @upcoming_appointment = current_user.appointment_hosts.where("start_time > ?", DateTime.now).where(status: "Paid").last || current_user.appointment_hosts.where("start_time > ?", DateTime.now).where(status: "Follow Up Unpaid").last
+
+      # If there is an upcoming appointment then set date
+      if @upcoming_appointment 
+        @upcoming_appointment.date = @upcoming_appointment.start_time.strftime("%A, %b %d") unless @upcoming_appointment.nil?
+        @upcoming_appointment.time = @upcoming_appointment.start_time.strftime("%I:%M%p") unless @upcoming_appointment.nil?
+      end
+  end
+
+  # Get last unpaid appointmnet for client
   def get_unpaid_appointment!
     # Set unpaid appointment if there is one
     @unpaid_appointment = current_user.appointment_hosts.where(status: "Follow Up Unpaid").last
   end
 
+  # Get appointment family info
   def get_appointment_family_info!
 
     @family = @appointment.appointment_host.head_of_families.first
@@ -105,14 +127,5 @@ module AppointmentsHelper
 
   end
 
-  # def get_family_info!
-  #   @family.health_groups_names = @family.health_groups
-  #   @family.age_groups = @family.ages
-  #   @family.number_of_members = @family.family_member_count
-  #   @family.family_names = @family.all_first_names
-  #   get_family_member_info!
-  #   @family.family_member_info = @family_members
-    
-  # end
 
 end
--- app/helpers/families_helper.rb
+++ app/helpers/families_helper.rb
@@ -18,35 +18,5 @@ module FamiliesHelper
       
 	end
 
-	# def get_family_patient_groups!
-		
-	# 	@family_patient_groups = []
-	# 	@family_members.map do | family_member |
-	# 		array = family_member.patient_groups
-	# 		@family_patient_groups += array
-	# 	end
-	# 	@family_patient_groups = @family_patient_groups.uniq.sort_by{|a| a.name.downcase}
-	# 	#Rails.logger.debug("MY FAMILY PATIENT GROUPS: #{@patient_groups}")
-	# end
-
-	# def get_family_allergens!
-	# 	@family_allergens = []
-	# 	@family_patient_groups.map do | patient_group |
-	# 		array = patient_group.allergens
-	# 		@family_allergens += array
-	# 	end
-	# 	@family_allergens = @family_allergens.uniq.sort_by{|a| a.name.downcase}
-	# 	#Rails.logger.debug("MY FAMILY ALLERGENS: #{@ufamily_allergens}")
-	# end
-
-	# def get_family_ingredients!
-	# 	@family_ingredients = []
-	# 	@family_allergens.map do | allergen |
-	# 		array = allergen.ingredients
-	# 		@family_ingredients += array
-	# 	end
-	# 	@family_ingredients = @family_ingredients.uniq.sort_by{|a| a.name.downcase}
-	# 	Rails.logger.debug("MY FAMILY INGREDIENTS: #{@family_ingredients}")
-	# end
 
 end

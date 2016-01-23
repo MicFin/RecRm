9e8b6d448d095279e228e34e1f74ce28eb67bd06
Michael Finneran <mrfinneran@gmail.com>
2016-01-22 22:35:54 -0500

    added cloud front for asset hosting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "dh5bguyrh098f.cloudfront.net"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.

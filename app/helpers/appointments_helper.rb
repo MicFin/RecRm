b53c218f8f0ff0509c42d08bec149e33ba31801c
Michael Finneran <mrfinneran@gmail.com>
2016-01-28 12:35:36 -0500

    solved n+1

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -50,7 +50,8 @@ module AppointmentsHelper
     end
 
     # Build family and add to appointments
-    list_of_appointments.includes(:appointment_host).map do |appointment| 
+    # only uses patient focus when a user not a dietitian
+    list_of_appointments.includes(:appointment_host).includes(:patient_focus).map do |appointment| 
       family = appointment.appointment_host.head_of_families.last 
       family.health_groups_names = family.health_groups.map(&:name)
       family.age_groups = family.ages

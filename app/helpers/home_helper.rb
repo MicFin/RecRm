daccbaf17d77fc2e51cc98891be74e6b83f3165c
Michael Finneran <mrfinneran@gmail.com>
2016-02-01 15:33:55 -0500

    refactored welcome#home view by creating partials and a home_helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,15 @@
 module HomeHelper
+  
+  def user_appointment_prompt(upcoming_appointment, unpaid_appointment)
+    
+    if upcoming_appointment && upcoming_appointment.status == "Paid"  
+      render "welcome/home/upcoming_confirmed_appointment", upcoming_appointment: upcoming_appointment 
+
+    elsif upcoming_appointment && upcoming_appointment.status ==  "Follow Up Unpaid"
+      render "welcome/home/upcoming_unconfirmed_appointment", upcoming_appointment: unpaid_appointment 
+
+    else 
+      render "welcome/home/no_upcoming_appointment" 
+    end 
+  end
 end

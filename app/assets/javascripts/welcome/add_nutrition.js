766cce9f3ec51919bb5a9a5159d363f310c3ba11
Michael Finneran <mrfinneran@gmail.com>
2016-02-11 04:41:55 -0500

    health group allergy check list was not triggering because of poor jury selectors

--- app/assets/javascripts/welcome/add_nutrition.js
+++ app/assets/javascripts/welcome/add_nutrition.js
@@ -8,9 +8,8 @@ Kindrdfood.welcome.addNutrition = {
 	init: function(){
 	
 		// input for patient groups and has a data-trigger of true
-		$("#health-group-container").on("click", "input[name='user[patient_group_ids][]'][data-triggers='true']", function(e){
-		
-	    if ( $("input[name='user[patient_group_ids][]'][data-triggers='true']").is(":checked") ) {
+		$("input[data-triggers='true']").on("click", function(e){
+	    if ( $("input[data-triggers='true']").is(":checked") ) {
 	        $('.triggers-container').removeClass("hidden");
 	    } 
 	    else {

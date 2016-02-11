0c49948dac500a1fe2d4bf640f146b47839cd4cf
Michael Finneran <mrfinneran@gmail.com>
2016-02-11 00:46:56 -0500

    update carousel for prep

--- app/assets/javascripts/bootstrap_overrides/carousel.js
+++ app/assets/javascripts/bootstrap_overrides/carousel.js
@@ -7,16 +7,16 @@ Kindrdfood.bootstrapOverrides = Kindrdfood.bootstrapOverrides || {};
 Kindrdfood.bootstrapOverrides.carousel = {
   init: function(){
   	var checkItem = function() {
-  		var $this;
-		  $this = $("#carousel-client-prep");
-		  if ($("#carousel-client-prep .carousel-inner .item:first").hasClass("active")) {
-		    $("a[data-slide='prev']").hide();
-		    $("a[data-slide='next']").show();
-		  } else if ($("#carousel-client-prep .carousel-inner .item:last").hasClass("active")) {
-		   	$("a[data-slide='next']").hide();
-		    $("a[data-slide='prev']").show();
+  		var prepPosition = $(".item.active").data("prep-position");
+
+		  if (prepPosition === "growth-chart") {
+		    $("a[data-slide='prev']").addClass("hidden");
+		    $("a[data-slide='next']").removeClass("hidden");
+		  } else if (prepPosition === "survey")  {
+		   	$("a[data-slide='next']").addClass("hidden");
+		    $("a[data-slide='prev']").removeClass("hidden");
 		  } else {
-		    $("a[data-slide='prev'], a[data-slide='next']").show();
+		    $("a[data-slide='prev'], a[data-slide='next']").removeClass("hidden");
 		  }
   	}; 
 		$('#carousel-client-prep').on('slid.bs.carousel', checkItem);

c1fb8d5a2757a662ebe88348a0d5a430b2c03831
Michael Finneran <mrfinneran@gmail.com>
2016-02-17 14:24:35 -0500

    refactored purchases model with helper and partials

---
+++ app/helpers/purchases_helper.rb
@@ -0,0 +1,34 @@
+
+
+module PurchasesHelper
+
+  def show_purchase_cost(user, purchase, purchasable) 
+    if (user.qol_referral == false) && (purchasable.status != "Unused Package Session")
+        render "purchases/purchases_modal/purchase_cost", user: user, purchase: purchase, purchasable: purchasable
+    end 
+  end
+
+  def show_purchase_coupon_response(purchase, purchasable)
+    if purchase.invoice_cost != purchase.invoice_price 
+      render "purchases/purchases_modal/coupon_applied", purchase: purchase, purchasable: purchasable
+    else
+      render "purchases/purchases_modal/coupon_not_applied", purchase: purchase, purchasable: purchasable
+    end
+  end
+
+  def show_payment_option(user, purchase, purchasable, time_slot, f)
+
+    if purchase.status == "Paid"
+      render "purchases/purchases_modal/already_paid"
+
+    elsif purchasable.status == "Unused Package Session"
+      render "purchases/purchases_modal/pay_with_package", purchasable: purchasable, f: f
+
+    elsif user.qol_referral == false
+      render "purchases/purchases_modal/credit_card_form", user: user, purchase: purchase, purchasable: purchasable, f: f
+
+    else
+      render "purchases/purchases_modal/default_stripe_token", f: f
+    end 
+  end
+end

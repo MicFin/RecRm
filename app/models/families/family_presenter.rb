c26db70e85caaa54c6744a613a112e62fba429d5
Michael Finneran <mrfinneran@gmail.com>
2016-03-10 00:55:20 -0500

    family controller indent and family presenter creation but not used

---
+++ app/models/families/family_presenter.rb
@@ -0,0 +1,14 @@
+module Families
+  class FamilyPresenter < SimpleDelegator
+
+    # def users
+    #   Users::UserPresenter.present(users)
+    # end
+
+    # # CLASS METHODS
+    # def self.present(families)
+    #   families.map { |family| Families::FamilyPresenter.new(family) }
+    # end
+
+  end
+end

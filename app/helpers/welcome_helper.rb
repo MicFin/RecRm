b24fac15dbf224cfcf8b4db3ac86237c6c435f41
Michael Finneran <mrfinneran@gmail.com>
2016-04-02 15:10:06 -0400

    needed to use respond_to to because using presenter model not model itself

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -3,10 +3,10 @@ module WelcomeHelper
   # Show prompt to join and prep, pay, or purchase a new session
   def user_appointment_prompt(upcoming_appointment, unpaid_appointment)
     
-    if upcoming_appointment && upcoming_appointment.status == "Paid"  
+    if upcoming_appointment && upcoming_appointment.respond_to?(:status) && upcoming_appointment.status == "Paid"  
       render "welcome/home/upcoming_confirmed_appointment", appointment: upcoming_appointment 
 
-    elsif upcoming_appointment && upcoming_appointment.status ==  "Follow Up Unpaid"
+    elsif upcoming_appointment && upcoming_appointment.respond_to?(:status) && upcoming_appointment.status ==  "Follow Up Unpaid"
       render "welcome/home/upcoming_unconfirmed_appointment", appointment: unpaid_appointment 
 
     else 

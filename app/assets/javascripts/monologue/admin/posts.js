505966cff34c2b0b0f3fb4aeaa2a39f469df97d1
Michael Finneran <mrfinneran@gmail.com>
2016-04-06 07:25:03 -0400

    moved admin post js from html to js

--- app/assets/javascripts/monologue/admin/posts.js
+++ app/assets/javascripts/monologue/admin/posts.js
@@ -1,5 +1,5 @@
 $(document).ready (function(){
-   
+
    var preview = {
       el: null,
       
@@ -55,8 +55,101 @@ $(document).ready (function(){
          $(document).off("keydown.post-preview");
       }
    }
-   
+   var setTable = {
+      init: function(){
+
+         // Create table
+         var table = $(".datatable").DataTable({
+
+           // select filter on footer
+          initComplete: function () {
+               this.api().columns().every( function () {
+                   var column = this;
+                   var select = $('<select><option value=""></option></select>')
+                       .appendTo( $(column.footer()).empty() )
+                       .on( 'change', function () {
+                           var val = $.fn.dataTable.util.escapeRegex(
+                               $(this).val()
+                           );
+
+                           column
+                               .search( val ? '^'+val+'$' : '', true, false )
+                               .draw();
+                       } );
+                   column.data().unique().sort().each( function ( d, j ) {
+                       select.append( '<option value="'+d+'">'+d+'</option>' )
+                   } );
+               } );
+            }
+         });
+
+         // Get dynamic num of columns
+         var numColumns = table.columns()[0].length;
+         var hideColumnLink, columnTitle;
+
+         // Loop through all columns
+         for (var i = 0, l = numColumns; i < l; i++ ) {
+         // Create Links to hide columns
+         columnTitle = $(table.columns( i ).header()).text();
+         hideColumnLink = i === l - 1 || i === 9 ? '<a class="toggle-vis" data-column="'+i+'">'+columnTitle+'</a>' : '<a class="toggle-vis" data-column="'+i+'">'+columnTitle+'</a> - ';
+         // append links to hide columns and hide tag columns to start
+         if ( i > 10) {
+           if (columnTitle !== "") {
+             $('.hide-tag-columns').append(hideColumnLink);
+           }
+           table.column(i).visible(false);
+         } else {
+           if (columnTitle !== "") {
+             $('.hide-general-columns').append(hideColumnLink);
+           }
+           $("a[data-column='" + i + "']").addClass("visible-column");
+         }
+         };
+
+         // Create link to show all tag columns
+         $(".show-all-tags").on("click", function(e){
+            e.preventDefault();
+            for (var i = 11, l = numColumns; i < l; i++ ) {
+              table.column(i).visible(true);  
+              $("a[data-column='" + i + "']").addClass("visible-column");
+            }
+            $(".show-all-tags, .hide-all-tags").toggleClass("hidden");
+
+         });
+
+         // Create link to hide all tag columns
+         $(".hide-all-tags").on("click", function(e){
+            e.preventDefault();
+            for (var i = 11, l = numColumns; i < l; i++ ) {
+              table.column(i).visible(false);  
+              $("a[data-column='" + i + "']").removeClass("visible-column");
+            }
+            $(".hide-all-tags, .show-all-tags").toggleClass("hidden");  
+         });
+
+         // When links are clicked then toggle column
+         $('a.toggle-vis').on( 'click', function (e) {
+           e.preventDefault();
+           // Get the column API object
+           var column = table.column( $(this).attr('data-column') );
+           // Toggle the visibility
+           column.visible( ! column.visible() );
+           // Toggle link color
+           $(this).toggleClass("visible-column");
+         });
+
+         // Show table
+         $(".datatable").removeClass("hidden");
+      }
+   }   
    
    // initialize preview.
    preview.init("[data-toggle='post-preview']");
+
+   // initialize table
+   setTable.init();
+
+
+
+
 })

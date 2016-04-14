e12a5999dcc5f33b99b732c87295d5a73f302bdb
Michael Finneran <mrfinneran@gmail.com>
2016-04-14 12:43:36 -0400

    created monologue post admin index tag and heading filters to show and hide columns

--- app/assets/javascripts/monologue/admin/posts.js
+++ app/assets/javascripts/monologue/admin/posts.js
@@ -1,6 +1,7 @@
 $(document).ready (function(){
 
-   var preview = {
+
+  var preview = {
       el: null,
       
       // initialize - bind events, etc.
@@ -54,102 +55,98 @@ $(document).ready (function(){
          $("body").attr("style", "overflow:auto;");
          $(document).off("keydown.post-preview");
       }
-   }
-   var setTable = {
-      init: function(){
+  }
+  var setTable = {
+    init: function(){
 
-         // Create table
-         var table = $(".datatable").DataTable({
+      // Create table
+      var table = $(".datatable").DataTable({
 
-           // select filter on footer
-          initComplete: function () {
-               this.api().columns().every( function () {
-                   var column = this;
-                   var select = $('<select><option value=""></option></select>')
-                       .appendTo( $(column.footer()).empty() )
-                       .on( 'change', function () {
-                           var val = $.fn.dataTable.util.escapeRegex(
-                               $(this).val()
-                           );
+        // select filter on footer
+        initComplete: function () {
+          this.api().columns().every( function () {
+            var column = this;
+            var select = $('<select><option value=""></option></select>')
+              .appendTo( $(column.footer()).empty() )
+              .on( 'change', function () {
+                var val = $.fn.dataTable.util.escapeRegex( $(this).val() );
+                column
+                .search( val ? '^'+val+'$' : '', true, false )
+                .draw();
+              });
+            column.data().unique().sort().each( function ( d, j ) {
+              select.append( '<option value="'+d+'">'+d+'</option>' )
+            });
+          });
+        }
+      });
 
-                           column
-                               .search( val ? '^'+val+'$' : '', true, false )
-                               .draw();
-                       } );
-                   column.data().unique().sort().each( function ( d, j ) {
-                       select.append( '<option value="'+d+'">'+d+'</option>' )
-                   } );
-               } );
-            }
-         });
+      // Get dynamic num of columns in entire table
+      var numColumns = table.columns()[0].length, objectOfCategories = {};
+      var hideColumnLink, columnTitle, tagCategory, contentType, showColumnBoolean;
 
-         // Get dynamic num of columns
-         var numColumns = table.columns()[0].length;
-         var hideColumnLink, columnTitle;
+      // Loop through all columns to get the tag-categories
+      for (var i = 0, l = numColumns; i < l; i++ ) {
+        tagCategory = $(table.columns( i ).footer()).attr("data-tag-category");
+        contentType = $(table.columns( i ).footer()).attr("data-content-type");
+        showColumnBoolean = (tagCategory === "basic_info");
 
-         // Loop through all columns
-         for (var i = 0, l = numColumns; i < l; i++ ) {
-         // Create Links to hide columns
-         columnTitle = $(table.columns( i ).header()).text();
-         hideColumnLink = i === l - 1 || i === 9 ? '<a class="toggle-vis" data-column="'+i+'">'+columnTitle+'</a>' : '<a class="toggle-vis" data-column="'+i+'">'+columnTitle+'</a> - ';
-         // append links to hide columns and hide tag columns to start
-         if ( i > 10) {
-           if (columnTitle !== "") {
-             $('.hide-tag-columns').append(hideColumnLink);
-           }
-           table.column(i).visible(false);
-         } else {
-           if (columnTitle !== "") {
-             $('.hide-general-columns').append(hideColumnLink);
-           }
-           $("a[data-column='" + i + "']").addClass("visible-column");
-         }
-         };
+        // if tagCategory key has not been added to object 
+        if ( tagCategory && !objectOfCategories.hasOwnProperty(tagCategory) ) {
 
-         // Create link to show all tag columns
-         $(".show-all-tags").on("click", function(e){
-            e.preventDefault();
-            for (var i = 11, l = numColumns; i < l; i++ ) {
-              table.column(i).visible(true);  
-              $("a[data-column='" + i + "']").addClass("visible-column");
-            }
-            $(".show-all-tags, .hide-all-tags").toggleClass("hidden");
+          // then add colunID to object 
+          objectOfCategories[tagCategory] = [i];
 
-         });
+          // and add link to screen
+          hideColumnLink = '<a class="toggle-vis" data-tag-category="'+tagCategory+'">'+tagCategory.toUpperCase().split('_').join(' ')+'</a> | ';
 
-         // Create link to hide all tag columns
-         $(".hide-all-tags").on("click", function(e){
-            e.preventDefault();
-            for (var i = 11, l = numColumns; i < l; i++ ) {
-              table.column(i).visible(false);  
-              $("a[data-column='" + i + "']").removeClass("visible-column");
-            }
-            $(".hide-all-tags, .show-all-tags").toggleClass("hidden");  
-         });
+          if ( contentType === "recipe") {
+            $('.hide-recipe-columns').append(hideColumnLink);
+          } else if ( contentType === "article") {
+            $('.hide-article-columns').append(hideColumnLink);
+          } else {
+            $('.hide-general-columns').append(hideColumnLink);
+          }
+          table.column(i).visible(showColumnBoolean);
+        // if tagCategory key has been added
+        } else if (tagCategory) {
 
-         // When links are clicked then toggle column
-         $('a.toggle-vis').on( 'click', function (e) {
-           e.preventDefault();
-           // Get the column API object
-           var column = table.column( $(this).attr('data-column') );
-           // Toggle the visibility
-           column.visible( ! column.visible() );
-           // Toggle link color
-           $(this).toggleClass("visible-column");
-         });
+          // then add columnID to object
+          objectOfCategories[tagCategory].push(i); 
+          table.column(i).visible(showColumnBoolean);           
+        }
+      };
 
-         // Show table
-         $(".datatable").removeClass("hidden");
-      }
-   }   
+      $(".toggle-vis").on("click", function(e){
+        e.preventDefault();
+        var $link = $(this);
+        var categoryName = $link.attr("data-tag-category");
+        var arrayOfColumnIds = objectOfCategories[categoryName];
+
+        if ( $link.hasClass("visible-column") ) {
+          $link.removeClass("visible-column");
+          arrayOfColumnIds.forEach(function(columnId){
+            table.column(columnId).visible(false);
+          });
+
+        } else {
+          $link.addClass("visible-column");
+          arrayOfColumnIds.forEach(function(columnId){
+            table.column(columnId).visible(true);
+          });
+        }
+      })
+
+      // Show table
+      $(".datatable").removeClass("hidden");
+    }
+  }   
    
-   // initialize preview.
-   preview.init("[data-toggle='post-preview']");
+  // initialize preview.
+  preview.init("[data-toggle='post-preview']");
 
-   // initialize table
-   setTable.init();
-
-
+  // initialize table
+  setTable.init();
 
 
 })

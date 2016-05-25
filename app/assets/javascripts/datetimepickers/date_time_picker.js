3a453ce6e8906cd3dcb2257fecf6da1336b5b36d
Michael Finneran <mrfinneran@gmail.com>
2016-05-25 11:51:59 -0400

    create datetimepicker with date only

--- app/assets/javascripts/datetimepickers/date_time_picker.js
+++ app/assets/javascripts/datetimepickers/date_time_picker.js
@@ -9,5 +9,11 @@ Kindrdfood.dateTimePickers.dateTimePicker = {
   init: function(){
   	$("#datetimepicker, .datetimepicker").datetimepicker({ sideBySide: true, allowInputToggle: true });
 
+	},
+	dateOnlyInit: function(){
+		$('#datepicker').datetimepicker({
+      format: 'DD/MM/YYYY',
+      allowInputToggle: true
+    });
 	}
 }

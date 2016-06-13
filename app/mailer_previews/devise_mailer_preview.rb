c4b4d51fb9d69a353bdcee95bcb36d6c40cfbbed
Michael Finneran <mrfinneran@gmail.com>
2016-06-13 04:36:13 -0400

    email preview updates

--- app/mailer_previews/devise_mailer_preview.rb
+++ app/mailer_previews/devise_mailer_preview.rb
@@ -1,35 +1,32 @@
 class DeviseMailerPreview
 
-  def confirmation_instructions
-
-    # devise_mail(record, :confirmation_instructions, opts)
-    mock_user = mock_user("Alice")
-    Devise::Mailer.confirmation_instructions(mock_user,"fdsafdsafsd")
-
-      # you can provide values to be interpolated with %{}, e.g. "Hello %{user}"
-
-  end
-
-  def reset_password
-    mock_user = mock_user("Alice")
-    Devise::Mailer.reset_password_instructions(mock_user,"fdsafdsafsd")
-  end
-
-  # # Not using unlock
-  # def unlock_instructions
-  #   mock_user = mock_user("Alice")
-  #   Devise::Mailer.unlock_instructions(mock_user,"fdsafdsafsd")
-  # end
   def invitation_from_provider
     # devise_mail(record, :invitation_instructions, opts)
     mock_user = mock_user("Alice")
     Devise::Mailer.invitation_instructions(mock_user,"fdsafdsafsd")
   end
 
+  # def confirmation_instructions
+
+  #   # devise_mail(record, :confirmation_instructions, opts)
+  #   mock_user = mock_user("Alice")
+  #   Devise::Mailer.confirmation_instructions(mock_user,"fdsafdsafsd")
+
+  #     # you can provide values to be interpolated with %{}, e.g. "Hello %{user}"
+
+  # end
+
+  # # Not using unlock
+  # def unlock_instructions
+  #   mock_user = mock_user("Alice")
+  #   Devise::Mailer.unlock_instructions(mock_user,"fdsafdsafsd")
+  # end
+
+
   def invitation_from_qol
     # devise_mail(record, :invitation_instructions, opts)
-    mock_user = mock_user("Alice")
-    Devise::Mailer.invitation_instructions(mock_user,"fdsafdsafsd")
+    mock_user = mock_user("Alice", true)
+    Devise::Mailer.confirmation_instructions(mock_user,"fdsafdsafsd")
   end
 
   def invitation_from_krdn
@@ -44,11 +41,16 @@ class DeviseMailerPreview
     Devise::Mailer.invitation_instructions(mock_user,"fdsafdsafsd")
   end
 
+  def reset_password
+    mock_user = mock_user("Alice")
+    Devise::Mailer.reset_password_instructions(mock_user,"fdsafdsafsd")
+  end
+  
   private
   # You can put all your mock helpers in a module
   # or you can use your factories / fabricators, just make sure you are not creating anything
-  def mock_user(name = 'Bill Gates')
-    fake_id User.new(first_name: name, email: "user#{rand 100}@test.com")
+  def mock_user(name = 'Bill Gates', qol_referral = false)
+    fake_id User.new(first_name: name, email: "user#{rand 100}@test.com", qol_referral: qol_referral)
   end
 
   def fake_id(obj)

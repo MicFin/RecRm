4a36bd049db3d5ff8d7db019c6c589a3e7f21b5a
Michael Finneran <mrfinneran@gmail.com>
2016-08-26 07:06:47 -0400

    remove old code

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -69,53 +69,7 @@ class SurveysController < ApplicationController
     
     respond_to do |format|
       if @survey.update(survey_params)
-        # @surveyable = @survey.surveyable
-        # @survey_type = @survey.survey_group.name
-        # @user_id = params[:user_id]
-        # @user = current_dietitian || current_user
-        
-        # if @survey.survey_group.name == "Dietitian Pre-Appointment"
-        #   @follow_up = Survey.generate_for_follow_up(@surveyable)
-        #   @appointment= @surveyable
-        #   @user_pre_appt_survey = Survey.generate_for_appointment(@appointment, @appointment.appointment_host)
-        #   @dietitian_pre_appt_survey = @appointment.surveys.where(survey_type: "Pre-Appointment-Dietitian").where(completed: true).last.questions.order("position")
-        #   @client = @appointment.appointment_host
-        #   # set @family before get_family_info
 
-        #   @family = @client.head_of_families.first
-        #   # get_family_info!
-        #   # @family_members
-        #   # @family
-        #   # create family should be a helper method on the family model
-        #   @family_members = []
-        #   if @appointment.patient_focus 
-        #     appointment_focus = @appointment.patient_focus
-        #     @family_members << appointment_focus
-        #   end
-        #   family_count = @family.users.count
-          
-        #   if family_count > 0
-        #     if @client != appointment_focus
-        #       @family_members << @client
-        #       @family.users.each do |family_member| 
-        #         if family_member != appointment_focus
-        #           @family_members << family_member 
-        #         end
-        #       end
-        #     else
-        #       @family.users.each do |family_member|
-        #           @family_members << family_member
-        #       end
-        #     end
-        #   else
-        #     @family_members << @client
-        #   end
-        #   get_patient_groups!
-        #   @diseases = @diseases 
-        #   @intolerances = @intolerances 
-        #   @allergies = @allergies
-        #   @diets =  @diets 
-        # end
         if current_user 
           format.html { redirect_to welcome_home_path, notice: 'Updated!' }
           format.json { render :show, status: :ok, location: @survey }

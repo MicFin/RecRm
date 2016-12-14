f7c3c463b79cd83f9690df9842b8d99cdbb267c4
Michael Finneran <mrfinneran@gmail.com>
2016-12-14 02:45:10 -0500

    add comments to dev environ

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,10 +54,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 
-  # http://mailcatcher.me/
+  # http://mailcatcher.me/ run mailcatcher and go to http://127.0.0.1:1080/
   config.action_mailer.smtp_settings = {:address => "localhost", :port => "1025"}
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
 
   # Initialize Bullet
   config.after_initialize do

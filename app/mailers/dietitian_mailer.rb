10a61344d11b7935efe0e58b0aa63c3184f89dc4
Michael Finneran <mrfinneran@gmail.com>
2016-12-14 02:55:30 -0500

    dietitian mailer preview bug

--- app/mailers/dietitian_mailer.rb
+++ app/mailers/dietitian_mailer.rb
@@ -1,7 +1,7 @@
 class DietitianMailer < ActionMailer::Base
   default :from => "admin@kindrdfood.com"
 
-  def new_appointment(dietitian_id)
+  def appointment_confirmation(dietitian_id)
     @dietitian = Dietitian.find(dietitian_id)
     mail(:to => @dietitian.email, :subject => "Welcome to Kindrdfood!")
   end

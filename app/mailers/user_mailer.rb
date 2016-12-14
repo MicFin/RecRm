9799d4ccbb9eb88323856af15c78f7bf3257209a
Michael Finneran <mrfinneran@gmail.com>
2016-12-14 02:47:59 -0500

    user mailer model

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,53 +1,57 @@
 class UserMailer < ActionMailer::Base
   default :from => "admin@kindrdfood.com"
 
-  def registration_confirmation(user_id)
-    @user = User.find(user_id)
+  def registration_confirmation(user)
+    @user = user
     mail(:to => @user.email, :subject => "Welcome to Kindrdfood!")
   end
 
-  def registration_one_day_reminder(user_id)
-    @user = User.find(user_id)
+  def registration_reminder_1_day(user)
+    @user = user
+    if @user.registration_stage < 6 
+      mail(:to => @user.email, :subject => "Welcome to Kindrdfood!")
+    end
+  end
+
+  def registration_reminder_1_week(user)
+    @user = user
+    if @user.registration_stage < 6 
+      mail(:to => @user.email, :subject => "Welcome to Kindrdfood!")
+    end
+  end
+
+  def appointment_confirmation(user)
+    @user = user
     mail(:to => @user.email, :subject => "Welcome to Kindrdfood!")
   end
 
-  def registration_one_week_reminder(user_id)
-    @user = User.find(user_id)
+  def appointment_reminder_3_day(user)
+    @user = user
     mail(:to => @user.email, :subject => "Welcome to Kindrdfood!")
   end
 
-  def new_appointment(user_id)
-    @user = User.find(user_id)
+  def appointment_reminder_1_day(user)
+    @user = user
     mail(:to => @user.email, :subject => "Welcome to Kindrdfood!")
   end
 
-  def appointment_reminder_3_day(user_id)
-    @user = User.find(user_id)
+  def appointment_reminder_1_hour(user)
+    @user = user
     mail(:to => @user.email, :subject => "Welcome to Kindrdfood!")
   end
 
-  def appointment_reminder_1_day(user_id)
-    @user = User.find(user_id)
+  def appointment_late_5_minutes(user)
+    @user = user
     mail(:to => @user.email, :subject => "Welcome to Kindrdfood!")
   end
 
-  def appointment_reminder_1_hour(user_id)
-    @user = User.find(user_id)
+  def appointment_missed(user)
+    @user = user
     mail(:to => @user.email, :subject => "Welcome to Kindrdfood!")
   end
 
-  def appointment_late_5_minutes(user_id)
-    @user = User.find(user_id)
-    mail(:to => @user.email, :subject => "Welcome to Kindrdfood!")
-  end
-
-  def appointment_missed(user_id)
-    @user = User.find(user_id)
-    mail(:to => @user.email, :subject => "Welcome to Kindrdfood!")
-  end
-
-  def assessment_ready(user_id)
-    @user = User.find(user_id)
+  def appointment_assessment_ready(user)
+    @user = user
     mail(:to => @user.email, :subject => "Welcome to Kindrdfood!")
   end
 

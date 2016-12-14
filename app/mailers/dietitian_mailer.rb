437e28b384f6ad399ff4c233a5bc259b3e9fe31a
Michael Finneran <mrfinneran@gmail.com>
2016-12-14 02:58:24 -0500

    dietitian mailer appointment confirmation needed to accept dietitian not id

--- app/mailers/dietitian_mailer.rb
+++ app/mailers/dietitian_mailer.rb
@@ -1,8 +1,8 @@
 class DietitianMailer < ActionMailer::Base
   default :from => "admin@kindrdfood.com"
 
-  def appointment_confirmation(dietitian_id)
-    @dietitian = Dietitian.find(dietitian_id)
+  def appointment_confirmation(dietitian)
+    @dietitian = dietitian
     mail(:to => @dietitian.email, :subject => "Welcome to Kindrdfood!")
   end
 

1a3c1acffd057f05f9e3df71e2b607a810cc506c
Michael Finneran <mrfinneran@gmail.com>
2017-01-08 15:56:08 -0500

    commented out sidekiq.rb trial configs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,18 +2,15 @@
 
 # if Rails.env.development?
   Sidekiq.configure_server do |config|
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-         Rails.application.config.database_configuration[Rails.env]
-
-    config['pool'] = Sidekiq.options[:concurrency] + 2
-     
-    ActiveRecord::Base.establish_connection(config)
-     
-    Rails.logger.debug("Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
+    # config = ActiveRecord::Base.configurations[Rails.env] ||
+    #      Rails.application.config.database_configuration[Rails.env]
+    # config['pool'] = Sidekiq.options[:concurrency] + 2     
+    # ActiveRecord::Base.establish_connection(config)     
+    # Rails.logger.debug("Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
 
     config.redis = { url: 'redis://localhost:6379/0', network_timeout: 5 }
   end
-  
+
   Sidekiq.configure_client do |config|
     config.redis = { url: 'redis://localhost:6379/0', network_timeout: 5 }
   end

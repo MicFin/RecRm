58b5e4bedb20375319f67a013727c092667d0fe1
Michael Finneran <mrfinneran@gmail.com>
2017-01-08 15:31:51 -0500

    attempt to fix redis email bug

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 # It is important to note that to configure the location of Redis, you must define both the Sidekiq.configure_server and Sidekiq.configure_client blocks. To do this put the following code into config/initializers/sidekiq.rb.
 
-if Rails.env.development?
+# if Rails.env.development?
   Sidekiq.configure_server do |config|
     config.redis = { url: 'redis://localhost:6379/0', network_timeout: 5 }
   end
@@ -8,16 +8,16 @@ if Rails.env.development?
     config.redis = { url: 'redis://localhost:6379/0', network_timeout: 5 }
   end
 
-else
+# else
 
-  Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://54.191.134.234:6379/0', network_timeout: 5 }
-  end
+#   Sidekiq.configure_server do |config|
+#     config.redis = { url: 'redis://54.191.134.234:6379/0', network_timeout: 5 }
+#   end
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://54.191.134.234:6379/0', network_timeout: 5 }
-  end
-end
+#   Sidekiq.configure_client do |config|
+#     config.redis = { url: 'redis://54.191.134.234:6379/0', network_timeout: 5 }
+#   end
+# end
 
 
 # NOTE: Unknown parameters are passed to the underlying Redis client so any parameters supported by the driver can go in the Hash.

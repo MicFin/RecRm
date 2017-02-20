184af36aec6904049e199822c7bde5b47a54b23a
Michael Finneran <mrfinneran@gmail.com>
2017-02-20 17:52:19 -0500

    versions views and table js

---
+++ app/assets/javascripts/versions.js
@@ -0,0 +1,11 @@
+// Controller specific JS to be executed for versions controller	
+
+// Versions Controller JS
+//= require versions/index
+
+$(document).ready(function() {
+
+	// JS for versinos#index
+	Kindrdfood.versions.index.init();
+
+});
